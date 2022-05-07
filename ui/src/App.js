c7b8450e758f1f4e24773bc480daa3d4b6dbfbd1
Винокуров Ян Павлович <pl8410@pl1.edu>
2022-05-07 15:49:36 +0500

    огурцы и помидоры с лучком

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 function App() {
-
+  
   const [notes, setNotes] = useState(null);
+  const [isUpdate, setIsUpdate] = useState(false);
+
+  const inputInfo = useRef(null);
+  const inputTitle = useRef(null);
 
   useEffect(() =>{
     axios.get(
@@ -20,10 +24,27 @@ function App() {
 
     );
 
-  }, [])
+  }, []);
+
+  const addNote = () => {
+    axios.post("http://localhost:9090/api/notes/add", {
+      title: inputTitle.current.value,
+      info: inputInfo.current.value
+    }, {
+      withCredintials: false
+    }).then(() => {
+      setIsUpdate(!isUpdate)
+    })
+  }
+
 
   return (
     <div className="App">
+      <label>gygiyg</label>
+      <input ref={inputInfo} type="text"/>
+      <label>opisanie</label>
+      <input ref={inputTitle} type="text"/>
+      <button onClick = {() => addNote()}>add</button>
       {!!notes && notes.map((note, index) => (
         <div key={index}>{note.title} - {note.info} </div>
       ))}
